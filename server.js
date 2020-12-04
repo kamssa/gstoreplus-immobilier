bb8a1e8e7476347a9096db498699f8b2ab4c5b77
kamssa <kamssa0@gmail.com>
2020-12-04 13:59:58 +0000

    ajout procfile

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express();
 const path = require("path");
 const request = require("request");
 app.use(express.static(__dirname + "dist/gstoreplusimmobilier"));
+
 app.listen(process.env.port || 4200, function(){
   console.log("up and running on port "+process.env.PORT);
 });
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname + "dist/gstoreplusimmobilier/index.html"));
+});
+var server = http.createServer((req, res) => {
+  //your stuff
 });
 console.log("app is listenning");
