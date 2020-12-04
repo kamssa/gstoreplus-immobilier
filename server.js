733cc8ed2bbfa286ba07efcd9f2d9a25094f8546
kamssa <kamssa0@gmail.com>
2020-12-04 07:01:17 +0000

    server.js revu

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require("path");
 const request = require("request");
 app.use(express.static(__dirname + "dist/gstoreplusimmobilier"));
-app.listen(process.env.port || 4200);
+app.listen(process.env.port || 4200, function(){
+  console.log("up and running on port "+process.env.PORT);
+});
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname + "dist/gstoreplusimmobilier/index.html"));
 });
