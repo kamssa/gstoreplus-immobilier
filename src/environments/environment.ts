b2b6ca4891adba1afb146f904df258545013884b
kamssa <kamssa0@gmail.com>
2020-12-04 04:49:20 +0000

    modif dr packade.json et env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
  // apiUrl: 'http://localhost:8080'
-  apiUrl: 'https://server-immobilier.herokuapp.com:8080'
+  apiUrl: 'https://server-immobilier.herokuapp.com'
 };
 
 /*
